e2d7949f874928fda603827cc9e6ba4b0351c7a9
rkschunk <rkschunk@gmail.com>
2017-10-24 00:01:17 -0200

    ViperBringup
    Change-Id: If1ba4cdfc1b1198b380613ef374a8bdabbb047f9

--- viper.mk
+++ viper.mk
@@ -1,9 +1,11 @@
 $(call inherit-product, device/motorola/falcon/full_falcon.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/viper/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := MOTO G
-PRODUCT_NAME := lineage_falcon
+PRODUCT_NAME := viper_falcon
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+VIPER_BUILD_TYPE=Final-N
